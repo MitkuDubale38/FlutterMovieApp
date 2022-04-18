0582a0789121019bf68c4c0dbb793cf1762be920
FlutLab.io <>
2022-04-18 13:36:32 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -81,7 +81,6 @@ class _MyAppState extends State<MyApp> {
                       );
                     }),
                 ElevatedButton(
-                  style: raisedButtonStyle,
                   onPressed: () {},
                   child: Text('Next'),
                 )
