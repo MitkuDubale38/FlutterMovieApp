0953cb6cd8bc9772a26588b2c9744b76252b6e24
FlutLab.io <>
2022-04-18 18:20:58 +0000

    Autosave

--- lib/movie_detail.dart
+++ lib/movie_detail.dart
@@ -33,7 +33,10 @@ class MovieDetail extends StatelessWidget {
               ]),
             ),
             Container(
-              width: MediaQuery.of(context).size.width,
+              padding: EdgeInsets.only(left: 16, right: 16 bottom:15),
+              child: Text("Overview", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+            ),
+            Container(
               padding: EdgeInsets.only(left: 16, right: 16),
               child: Text(movie.results[index].overview, textAlign: TextAlign.justify, style: TextStyle(fontSize: 16)),
             ),
