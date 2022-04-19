8c3dd9250b2583226147dc753d37d3d515c7956c
FlutLab.io <>
2022-04-19 10:38:38 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,7 @@ class _MyAppState extends State<MyApp> {
       home:Container(
       color: Colors.white,
       child:FlutterLogo(size:MediaQuery.of(context).size.height)
-    );
+    ),
     );
     );
   }
