d9ec4cb1cbe0eb8594e530cb453db7bec499ef12
FlutLab.io <>
2022-04-19 11:00:28 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -17,18 +17,13 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-
   void initState() {
     super.initState();
-    Timer(Duration(seconds: 3),
-    ()=>Navigator.pushReplacement(context,MaterialPageRoute(builder:(context) => HomePage()))
-         );
+    Timer(Duration(seconds: 3), () => Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => HomePage())));
   }
-
 
   @override
   Widget build(BuildContext context) {
-   
     return MaterialApp(
       // Application name
       debugShowCheckedModeBanner: false,
@@ -36,12 +31,8 @@ class _MyAppState extends State<MyApp> {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home:Container(
-      color: Colors.white,
-      child:FlutterLogo(size:MediaQuery.of(context).size.height)
-    ),
-    )
-    )
+      home: Container(color: Colors.white, child: FlutterLogo(size: MediaQuery.of(context).size.height)),
+    );
   }
 }
 
@@ -53,7 +44,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-    PostsModel? posts;
+  PostsModel? posts;
   bool isLoaded = false;
   int page = 1;
   final String iconBase = 'https://image.tmdb.org/t/p/w92/';
@@ -78,99 +69,99 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-     NetworkImage image;
+    NetworkImage image;
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Upcoming Movies"),
-        ),
-        body: Visibility(
-          visible: isLoaded,
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                RefreshIndicator(
-                  color: Colors.blue,
-                  triggerMode: RefreshIndicatorTriggerMode.anywhere,
-                  onRefresh: () async {
-                    await Future.delayed(Duration(seconds: 2));
-                    getData();
-                  },
-                  child: ListView.builder(
-                      shrinkWrap: true,
-                      itemCount: posts?.results.length,
-                      itemBuilder: (BuildContext context, int position) {
-                        if (posts!.results[position].posterPath != null) {
-                          image = NetworkImage(iconBase + posts!.results[position].posterPath);
-                        } else {
-                          image = NetworkImage(defaultImage);
-                        }
-                        final postions = posts!.results;
-                        return Dismissible(
-                          key: UniqueKey(),
-                          onDismissed: (direction) {
-                            ScaffoldMessenger.of(context).showSnackBar(SnackBar(backgroundColor: Colors.blue, content: Text('${posts!.results[position].title} Dismissed')));
-                            setState(() {
-                              postions.removeAt(position);
-                            });
-                          },
-                          child: Card(
+      appBar: AppBar(
+        title: Text("Upcoming Movies"),
+      ),
+      body: Visibility(
+        visible: isLoaded,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              RefreshIndicator(
+                color: Colors.blue,
+                triggerMode: RefreshIndicatorTriggerMode.anywhere,
+                onRefresh: () async {
+                  await Future.delayed(Duration(seconds: 2));
+                  getData();
+                },
+                child: ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: posts?.results.length,
+                    itemBuilder: (BuildContext context, int position) {
+                      if (posts!.results[position].posterPath != null) {
+                        image = NetworkImage(iconBase + posts!.results[position].posterPath);
+                      } else {
+                        image = NetworkImage(defaultImage);
+                      }
+                      final postions = posts!.results;
+                      return Dismissible(
+                        key: UniqueKey(),
+                        onDismissed: (direction) {
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(backgroundColor: Colors.blue, content: Text('${posts!.results[position].title} Dismissed')));
+                          setState(() {
+                            postions.removeAt(position);
+                          });
+                        },
+                        child: Card(
+                          color: Colors.white,
+                          elevation: 2.0,
+                          child: ListTile(
+                            onTap: () {
+                              MaterialPageRoute route = MaterialPageRoute(
+                                  builder: (_) => MovieDetail(
+                                        movie: posts as PostsModel,
+                                        index: position,
+                                      ));
+                              Navigator.push(context, route);
+                            },
+                            leading: CircleAvatar(
+                              backgroundImage: image,
+                            ),
+                            title: Text(posts!.results[position].title),
+                            subtitle: Text('Released: ' + posts!.results[position].title + ' - ' + posts!.results[position].voteAverage.toString() + ' ⭐'),
+                          ),
+                        ),
+                        background: Container(
+                          color: Colors.red[400],
+                          margin: const EdgeInsets.symmetric(horizontal: 5),
+                          alignment: Alignment.centerRight,
+                          child: const Icon(
+                            Icons.delete,
                             color: Colors.white,
-                            elevation: 2.0,
-                            child: ListTile(
-                              onTap: () {
-                                MaterialPageRoute route = MaterialPageRoute(
-                                    builder: (_) => MovieDetail(
-                                          movie: posts as PostsModel,
-                                          index: position,
-                                        ));
-                                Navigator.push(context, route);
-                              },
-                              leading: CircleAvatar(
-                                backgroundImage: image,
-                              ),
-                              title: Text(posts!.results[position].title),
-                              subtitle: Text('Released: ' + posts!.results[position].title + ' - ' + posts!.results[position].voteAverage.toString() + ' ⭐'),
-                            ),
                           ),
-                          background: Container(
-                            color: Colors.red[400],
-                            margin: const EdgeInsets.symmetric(horizontal: 5),
-                            alignment: Alignment.centerRight,
-                            child: const Icon(
-                              Icons.delete,
-                              color: Colors.white,
-                            ),
-                          ),
-                        );
-                      }),
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(vertical: 20),
-                  child: ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        // if (page != posts?.total_pages) {
-                        page = page + 1;
-                        getData();
-                        //}
-                      });
-                    },
-                    child: Text('More Movies'),
-                    style: ElevatedButton.styleFrom(
-                      elevation: 5,
-                      padding: EdgeInsets.symmetric(horizontal: 100, vertical: 20),
-                      primary: Colors.blue,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(50),
-                      ),
+                        ),
+                      );
+                    }),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 20),
+                child: ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      // if (page != posts?.total_pages) {
+                      page = page + 1;
+                      getData();
+                      //}
+                    });
+                  },
+                  child: Text('More Movies'),
+                  style: ElevatedButton.styleFrom(
+                    elevation: 5,
+                    padding: EdgeInsets.symmetric(horizontal: 100, vertical: 20),
+                    primary: Colors.blue,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50),
                     ),
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
-          replacement: const Center(child: CircularProgressIndicator()),
         ),
-      );
+        replacement: const Center(child: CircularProgressIndicator()),
+      ),
+    );
   }
 }
