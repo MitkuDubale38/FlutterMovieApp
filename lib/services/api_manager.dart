c25ad93300e9acffaf7b5e102e94d5bb68a44937
FlutLab.io <>
2022-04-20 11:51:56 +0000

    Autosave

--- lib/services/api_manager.dart
+++ lib/services/api_manager.dart
@@ -11,15 +11,6 @@ class ApiManager {
     return PostsModel.fromJson(jsonDecoded);
   }
 
-  Future<List> findMovies(String title) async {
-    http.Response result = await http.get("https://api.themoviedb.org/3/search/movie?api_key=4ace3c380c51fa8a8483e1e93cdc0160&query=$title");
-    if (result.statusCode == HttpStatus.ok) {
-      final jsonResponse = json.decode(result.body);
-      final moviesMap = jsonResponse['results'];
-      List movies = moviesMap.map((i) => Movie.fromJson(i)).toList();
-      return movies;
-    } else {
-      return null;
-    }
+
   }
 }
